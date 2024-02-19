828965d447e2e1313d8398830ce16746bbd53563
Michał Wcisło <wcislom@gmail.com>
2024-02-19 10:01:25 +0100

    Adds Activity example

--- src/BookStore/BookStore.Api/Controllers/BooksController.cs
+++ src/BookStore/BookStore.Api/Controllers/BooksController.cs
@@ -4,6 +4,7 @@ using BookStore.Application.Queries;
 using BookStore.Application.Queries.DTO;
 using Microsoft.AspNetCore.Mvc;
 using Shared.Abstractions.CQRS;
+using System.Diagnostics;
 
 namespace BookStore.Api.Controllers;
 
@@ -14,6 +15,7 @@ public class BooksController : ControllerBase
     private readonly ILogger<BooksController> _logger;
     private readonly IQueryHandler<GetBooks, IEnumerable<BookDto>> _queryHandler;
     private readonly ICommandHandler<CreateBook> _createBook;
+    private readonly ActivitySource _activitySource;
 
     public BooksController(ILogger<BooksController> logger, IQueryHandler<GetBooks,
         IEnumerable<BookDto>> queryHandler,
@@ -22,12 +24,15 @@ public class BooksController : ControllerBase
         _logger = logger;
         _queryHandler = queryHandler;
         _createBook = createBook;
+        _activitySource = new ActivitySource("SkillUp.BookStore.Controller");
+
     }
 
     [HttpGet(Name = "GetBooksWithAuthors")]
     public async Task<IActionResult> Get()
     {
         using(_logger.BeginScope("BooksController.Get for user {userId}", User.Identity?.Name))
+        using(var activity = _activitySource.StartActivity("BrowseBooks"))
         {
             _logger.LogInformation("Getting books with authors");
             return Ok(await _queryHandler.HandleAsync(new GetBooks()));
